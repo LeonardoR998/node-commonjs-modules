a389f5fd951e8a381878688b19ba7eb0e81a79f5
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-18 14:46:50 +0100

    add function names.js

---
+++ names.js
@@ -0,0 +1,7 @@
+// Funzione che accetta due parametri e restituisce un oggetto
+function createName(firstName, lastName) {
+  return { firstName, lastName };
+}
+
+// Esportiamo la funzione
+module.exports = createName;
